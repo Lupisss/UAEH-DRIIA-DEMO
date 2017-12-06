bf6c29cf51af4462b3f7f08bd232190d82063c48
Guadalupe <lupiz_1810@hotmail.com>
2017-12-06 12:44:07 -0600

    Modificación en subir archivos

--- src/components/loadfiles/LoadFilesComponent.js
+++ src/components/loadfiles/LoadFilesComponent.js
@@ -2,16 +2,30 @@ import React, {Fragment} from "react";
 import { Tabs, Tab, Paper, RaisedButton} from 'material-ui';
 import {UploadFile} from "./UploadFile"
 
+const style = {
+    height: 100,
+    width: 100,
+    margin: 20,
+    textAlign: 'center',
+    display: 'inline-block',
+};
+
 export const LoadFilesComponent = (props) => {
     return (
-        <Paper className="Main-form-tp" zDepth={3}>
-            <form  style={{textAlign:'left'}}>
-                <UploadFile
+        <div>
+            <Paper  className="Main-form-tp"  style={style} zDepth={4} circle={true} >
+
+                <form   style={{textAlign:'left'}} >
+
+                    <UploadFile
                     fileName="CURP"
                     fileType=".pdf"
                     className="uploadfile"
                 />
-            </form>
-        </Paper>
+
+                </form>
+
+            </Paper>
+        </div>
     );
 };
