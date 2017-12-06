432a4f8ef0272d203c9995e4d0a4948436d1f246
Miguel Rafael González Durón <miglez90290995@gmail.com>
2017-12-06 16:26:24 -0600

    Creacion de tabla y buscador en admin

--- src/components/admin/AdminComponent.js
+++ src/components/admin/AdminComponent.js
@@ -1,9 +1,71 @@
 import React from 'react';
+import {
+    Table,
+    TableBody,
+    TableFooter,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
+    TextField
+} from 'material-ui';
+import './AdminStylesheet.css';
 
 export const AdminComponent = ({props}) => {
     return (
         <div>
-            yo soy el admin
+            <div className="search">
+                <TextField
+                    style={{width:'50%'}}
+                    className="search-text"
+                    hintText="Busca..."
+                    name="search"
+                />
+            </div>
+            <Table>
+                <TableHeader>
+                    <TableRow >
+                        <TableHeaderColumn colSpan={3} tooltip="Lista de alumnos">
+                            Lista de alumnos
+                        </TableHeaderColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableHeaderColumn>No Cuenta</TableHeaderColumn>
+                        <TableHeaderColumn>Nombre completo</TableHeaderColumn>
+                        <TableHeaderColumn>Universidad destino</TableHeaderColumn>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableRowColumn>244755</TableRowColumn>
+                        <TableRowColumn>Miguel Rafael Gonzalez Duron</TableRowColumn>
+                        <TableRowColumn>MSSU</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>244755</TableRowColumn>
+                        <TableRowColumn>Miguel Rafael Gonzalez Duron</TableRowColumn>
+                        <TableRowColumn>MSSU</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>244755</TableRowColumn>
+                        <TableRowColumn>Miguel Rafael Gonzalez Duron</TableRowColumn>
+                        <TableRowColumn>MSSU</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>244755</TableRowColumn>
+                        <TableRowColumn>Miguel Rafael Gonzalez Duron</TableRowColumn>
+                        <TableRowColumn>MSSU</TableRowColumn>
+                    </TableRow>
+                    <TableRow>
+                        <TableRowColumn>244755</TableRowColumn>
+                        <TableRowColumn>Miguel Rafael Gonzalez Duron</TableRowColumn>
+                        <TableRowColumn>MSSU</TableRowColumn>
+                    </TableRow>
+                </TableBody>
+                <TableFooter>
+
+                </TableFooter>
+            </Table>
         </div>
     );
 };
