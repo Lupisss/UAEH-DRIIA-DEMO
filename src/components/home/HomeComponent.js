46cd417036e90de7dc799a7c4d2e549558b42ea8
Miguel Gonzalez <miglez90290995@gmail.com>
2018-02-19 14:33:03 -0600

    assets and updates

--- src/components/home/HomeComponent.js
+++ src/components/home/HomeComponent.js
@@ -11,7 +11,7 @@ export const HomeComponent =() => (
         <Slide />
         <About />
         <Publi />
-        <Categorias />
+        {/*<Categorias />*/}
         <Public />
         <Info />
     </div>
