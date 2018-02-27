25aa9877d52669874da0f4f69be2d28035963a7b
Miguel Gonzalez <miglez90290995@gmail.com>
2018-02-26 21:00:54 -0600

    api issues

--- src/api/API.js
+++ src/api/API.js
@@ -1,5 +1,6 @@
+// importar axios from axios
 import axios from 'axios';
-
+// Variable para determinar si es local o e producción
 let debug = true;
 
 //Working on localhost
@@ -8,6 +9,7 @@ let loginUrl = 'http://localhost:8000/rest-auth/login/';
 let signupUrl = 'http://localhost:8000/rest-auth/registration/';
 let logoutUrl = 'http://localhost:8000/rest-auth/logout/';
 let collegesUrl = 'http://localhost:8000/api/collegeapi/';
+let profileUrl = 'http://localhost:8000/api/publicprofile/';
 // Production urls
 if (!debug) {
     //let collegesUrl = 'http://uaeh.herokuapp.com/api/collegeapi';
@@ -15,64 +17,103 @@ if (!debug) {
 
 const tokenName = 'user_uaeh_token';
 // Axios functions
+// Crear un objeto de funciones
+// Necesitamos comunicarnos con el servidor,
+// axios se encarga de gestionar dicha comunicación
+// clave valor, donde la clave es el nombre y el valor
+// es una función o arrow function
 export const api = {
     /** User functions  **/
     logIn: user => {
-        return new Promise( (resolve, reject) => {
+        // Retornar una nueva promesa, la promesa es como un try catch
+        // el constructor recibe una funcion (arrow function)
+        // donde recibe resolve y reject
+        return new Promise((resolve, reject) => {
+            // Creamos una instancia de axios, para poder
+            // comunicarnos con el servidor
+            // axios.create recibe un objeto en donde pasamos
+            // baseURL y headers
             const instance = axios.create({
+                //baseURL es la url del endpoint
                 baseURL: loginUrl,
+                // headers es lo necesario para hacer la petición
                 headers: {
-                    'Content-Type' : 'application/json'
+                    'Content-Type': 'application/json'
+                }
+            });
+            // Ejecutamos un método http, el primer argumento es
+            // la url (si queremos agregar algo más a la url)
+            // el segundo es el objeto con la información
+            // necesaria
+            instance.post('', user)
+                .then(r => {
+                    // si es correcto
+                    // guardamos el resultado (el token de Django)
+                    // en la base de datos (Local Storage)
+                    // El primer argumento es el nombre con el que se va a guardar
+                    // El segundo es la información que se guardara
+                    // Siempre debe hacer una llamada a JSON.stringify
+                    localStorage.setItem(tokenName, JSON.stringify(r.data.key));
+                    // si es correcto, mediante resolve, retornamos la Promise
+                    resolve(r.data);
+                }).catch(e => {
+                console.log(e);
+                // Si algo salio mal, mediante reject, retornamos la promise
+                reject(e.response);
+            });
+        });
+    },
+    signUp: user => {
+        return new Promise((resolve, reject) => {
+            const instance = axios.create({
+                baseURL: signupUrl,
+                headers: {
+                    'Content-Type': 'application/json'
                 }
             });
             instance.post('', user)
-                .then( r => {
+                .then(r => {
                     localStorage.setItem(tokenName, JSON.stringify(r.data.key));
                     resolve(r.data);
-                }).catch( e => {
-                    console.log(e);
-                    reject(e.response);
+                }).catch(e => {
+                console.log(e);
+                reject(e.response);
             });
         });
     },
-    signUp : user => {
-        return new Promise( (resolve,reject) => {
-           const instance = axios.create({
-               baseURL: signupUrl,
-               headers: {
-                   'Content-Type' : 'application/json'
-               }
-           });
-           instance.post('', user)
-               .then( r => {
-                   localStorage.setItem(tokenName, JSON.stringify(r.data.key));
-                   resolve(r.data);
-               }).catch( e => {
-                    console.log(e);
-                    reject(e.response);
-           });
-        });
-    },
-    logOut : () => {
-        return new Promise( (resolve, reject) => {
+    logOut: () => {
+        return new Promise((resolve, reject) => {
             const instance = axios.create({
                 baseURL: logoutUrl,
                 headers: {
-                    'Content-Type' : 'application/json'
+                    'Content-Type': 'application/json'
                 }
             });
-            instance.post('',{})
+            instance.post('', {})
                 .then(r => {
                     localStorage.removeItem(tokenName);
                     resolve(r.data);
                 }).catch(e => {
-                    reject(e.response);
+                reject(e.response);
+            });
+        });
+    },
+    setProfile: profile => {
+        return new Promise((resolve, reject) => {
+            const instance = axios.create({
+                baseURL: profileUrl
+            });
+            instance.post('', profile)
+                .then(r => {
+
+                }).catch(e => {
+
             });
         });
     },
     /** End user functions **/
     getColleges: () => {
-        return new Promise( (resolve, reject) => {
+        return new Promise((resolve, reject) => {
             const instance = axios.create({
                 baseURL: collegesUrl,
                 headers: {
@@ -80,53 +121,53 @@ export const api = {
                 }
             });
             instance.get()
-                .then( r => {
+                .then(r => {
                     console.log(r);
                     resolve(r.data)
-                }).catch( error => {
-                    console.error(error);
-                    reject(error.response)
+                }).catch(error => {
+                console.error(error);
+                reject(error.response)
             });
         });
     },
-    newCollege : (college) => {
-        return new Promise( (resolve, reject) => {
+    newCollege: (college) => {
+        return new Promise((resolve, reject) => {
             const instance = axios.create({
                 baseURL: collegesUrl,
                 headers: {
                     'Content-Type': 'application/json'
                 }
             });
-            instance.post('',college)
-                .then( r => {
+            instance.post('', college)
+                .then(r => {
                     console.log(r);
                     resolve(r.data)
-                }).catch( error => {
+                }).catch(error => {
                 console.error(error);
                 reject(error.response)
             })
         })
     },
-    updateCollege : (college) => {
-        return new Promise( (resolve, reject) => {
+    updateCollege: (college) => {
+        return new Promise((resolve, reject) => {
             const instance = axios.create({
                 baseURL: collegesUrl,
                 headers: {
                     'Content-Type': 'application/json'
                 }
             });
-            instance.patch(college.id + '/',college)
-                .then( r => {
+            instance.patch(college.id + '/', college)
+                .then(r => {
                     console.log(r);
                     resolve(r.data)
-                }).catch( error => {
+                }).catch(error => {
                 console.error(error);
                 reject(error.response)
             })
         })
     },
     removeCollege: (id) => {
-        return new Promise( (resolve, reject) => {
+        return new Promise((resolve, reject) => {
             const instance = axios.create({
                 baseURL: collegesUrl,
                 headers: {
@@ -134,10 +175,10 @@ export const api = {
                 }
             });
             instance.delete(id + '/')
-                .then( r => {
+                .then(r => {
                     console.log(r);
                     resolve(r.data)
-                }).catch( error => {
+                }).catch(error => {
                 console.error(error);
                 reject(error.response)
             })
