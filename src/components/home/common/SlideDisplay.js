cc2b6a22b8c2db1852cff289f2ae1863facf469e
Miguel Gonzalez <miglez90290995@gmail.com>
2018-03-07 09:47:59 -0600

    Video instead photo in main page

--- src/components/home/common/SlideDisplay.js
+++ src/components/home/common/SlideDisplay.js
@@ -6,10 +6,14 @@ import {RaisedButton} from 'material-ui';
 
 //Texto y boton sobre imagen principal
 export const SlideDisplay = () => {
+    const video = require('../../../assets/mainvideo2.mp4');
     return (
         <div className="slide">
             <div className="cover">
                 <div className="descript">
+                    <video autoPlay muted loop id="myVideo">
+                        <source src={video} type="video/mp4"/>
+                    </video>
 
                     <div className="slogan">
                         <h2>Lorem ipsum dolor sit amet, consectetur</h2>
